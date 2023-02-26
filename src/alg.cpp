d5b38a4f97870a21ea849638120bf53c3c66425e
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-02-26 23:36:38 +0300

    hmmm

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,35 @@
-// Copyright 2022 NNTU-CS
-#include <cstdint>
-#include "alg.h"
-
-
-bool checkPrime(uint64_t value) {
-// вставьте код функции
-}
-
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int znach(int *a,int n){
+    for int i=0; i<n: i++)
+      if(n==1 || a[i]==2 || a[i]==3 || a[i]==5 || a[i]==7)
+        return true;
+    else if(a[i]%2!=0 && a[i]%3!=0 && a[i]%5!=0 && a[i]%7!=0)
+      return a[i];
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  int znach(int n){
+    int count=0
+      if(count!=0)
+        if(n==1 || a[i]==2 || a[i]==3 || a[i]==5 || a[i]==7)
+          return n;
+        else if(n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0)
+          return n;
+    count+=1;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int znach(int n){
+    int sum=0;
+    if(n==1 || a[i]==2 || a[i]==3 || a[i]==5 || a[i]==7)
+      sum+=n;
+      return n+1;
+    else if(n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0)
+      sum+=n;
+    return n+1;
+    else
+      return n+1;
+  }
 }
