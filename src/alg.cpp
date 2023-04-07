d2288fc197edec447057a5ea1d99f2f47ec216d0
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-04-07 17:00:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,35 +1,36 @@
-uint64_t nPrime(uint64_t n) {
-  int znach(int *a,int n){
-    for int i=0; i<n: i++)
-      if(n==1 || a[i]==2 || a[i]==3 || a[i]==5 || a[i]==7)
-        return true;
-    else if(a[i]%2!=0 && a[i]%3!=0 && a[i]%5!=0 && a[i]%7!=0)
-      return a[i];
+bool checkPrime(uint64_t value) {
+  if (value <= 1)
+    return false;
+  for (int i = 2; i * i <= value; i++) {
+    if (value % i == 0)
+      return false;
   }
+  return true;
+}
+
+uint64_t nPrime(uint64_t n) {
+  uint64_t q = 2;
+    for (uint64_t i = 0; i < n; ++q)
+    if (checkPrime(q))
+            i++;
+    return (q - 1);
 }
 
 uint64_t nextPrime(uint64_t value) {
-  int znach(int n){
-    int count=0
-      if(count!=0)
-        if(n==1 || a[i]==2 || a[i]==3 || a[i]==5 || a[i]==7)
-          return n;
-        else if(n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0)
-          return n;
-    count+=1;
+  uint64_t q = value + 1;
+  while (true) {
+    if (checkPrime(q))
+      return q;
+    else
+      ++q;
   }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int znach(int n){
-    int sum=0;
-    if(n==1 || a[i]==2 || a[i]==3 || a[i]==5 || a[i]==7)
-      sum+=n;
-      return n+1;
-    else if(n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0)
-      sum+=n;
-    return n+1;
-    else
-      return n+1;
+  uint64_t a = 0;
+  for (uint64_t n = hbound - 1; n > 1; --n) {
+    if (checkPrime(n))
+      a += n;
   }
+  return a;
 }
